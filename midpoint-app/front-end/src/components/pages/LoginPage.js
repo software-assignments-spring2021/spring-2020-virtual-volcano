aa9bec4f2e4a984604d387cfdd6579e5ab80ecdb
Tiffany <tiffany@boxed.com>
2020-03-26 01:15:37 -0400

    added login and homepage

---
+++ midpoint-app/front-end/src/components/pages/LoginPage.js
@@ -0,0 +1,22 @@
+import React from "react";
+import LoginForm from '../forms/LoginForm'
+
+class LoginPage extends React.Component {
+
+    submit = (data) => {
+       // just to make sure we actually get data 
+        console.log(data);
+    };
+
+    render() {
+        return (
+            <div>
+                <h1>Login Page</h1>
+
+                <LoginForm submit={this.submit}/>
+            </div>
+        )
+    }
+}
+
+export default LoginPage;
