d908b64e07919ba81457d15e2d5d811e9d5ee9a0
Valentine Choi <jhc591@nyu.edu>
2020-03-26 23:19:03 -0400

    map testing

--- midpoint-app/front-end/src/components/forms/HomeForm.js
+++ midpoint-app/front-end/src/components/forms/HomeForm.js
@@ -94,6 +94,7 @@ class HomeForm extends React.Component {
                 <Link to='/area'><Button primary>Distance</Button></Link>
                 <Link to='/area'><Button primary>Price</Button></Link>
 
+
                 <p></p>
 
                 {/*<MapContainer />*/}
