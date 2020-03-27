407a5455dbfbc4ad34e5a434cbe6e75e09918b54
Valentine Choi <jhc591@nyu.edu>
2020-03-26 23:21:29 -0400

    map testing

--- midpoint-app/front-end/src/components/pages/AreaPage.js
+++ midpoint-app/front-end/src/components/pages/AreaPage.js
@@ -4,15 +4,15 @@ import Header from '../template/Header';
 import AreaForm from '../forms/AreaForm';
 
 function AreaPage(props) {
-    return (
-        <div>
-            <Header />
-            <AreaForm />
-            {/* Link to final calculated midpoint page instead of login */}
-            {/* <Link to='/login'>Login</Link> */}
-            <Footer />
-        </div>
-    )
+        return (
+            <div>
+                <Header />
+                <AreaForm />
+                {/* Link to final calculated midpoint page instead of login */}
+                {/* <Link to='/login'>Login</Link> */}
+                <Footer />
+            </div>
+        )
 }
 
 export default AreaPage;
