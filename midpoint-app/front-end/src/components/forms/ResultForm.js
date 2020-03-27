1ce3d7299ac0be016f2fef661e77418e05c2cab1
Valentine Choi <jhc591@nyu.edu>
2020-03-27 00:09:39 -0400

    created result page

---
+++ midpoint-app/front-end/src/components/forms/ResultForm.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Form, Button, ButtonGroup } from 'semantic-ui-react';
+import Dropdown from 'react-dropdown';
+import { Link } from "react-router-dom";
+
+
+function ResultForm(props) {
+    const resultOptions = [
+        'Open in \'Maps\'', 'Open in \'Google Maps\''
+    ];
+
+    return (
+        <Form className="location-box">
+            <p>Estimated Journey to your destination</p>
+            <ButtonGroup aria-label="Basic example">
+                <Button variant="primary">15 minutes</Button>
+                <Button variant="primary">0.7 miles</Button>
+                <Button variant="primary">$2.75</Button>
+            </ButtonGroup>
+            <p></p>
+            <Dropdown options={resultOptions} placeholder="Get Directions" />
+            <p></p>
+            <img src="https://picsum.photos/500/300?grayscale" />
+        </Form>
+    )
+}
+
+export default ResultForm;
