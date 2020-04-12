c48f5fe8ccdce0e105da103e85d5ca24f1eaa387
Niki <54921767+literallyniki@users.noreply.github.com>
2020-04-12 16:27:53 -0400

    just taking these out for now, will update later

--- midpoint-app/front-end/src/components/pages/AreaPage.js
+++ midpoint-app/front-end/src/components/pages/AreaPage.js
@@ -3,19 +3,15 @@ import Footer from '../template/Footer';
 import Header from '../template/Header';
 import AreaForm from '../forms/AreaForm';
 import MapContainer from '../forms/MapContainer';
-// import SideBar from "../../sidebar";
 import { Link } from "react-router-dom";
-//import { BrowserRouter } as Router from "react-router-dom";
 
 class AreaPage extends Component {
     render() {
         return (
-            //<Router>
             <div>
                 <Header />
                 <AreaForm />
                 {/* Link to final calculated midpoint page instead of login */}
-               // <Link to='login'>Login</Link>
                 {/* <Link to='/login'>Login</Link> */}
                 <MapContainer
                     google={this.props.google}
@@ -23,9 +19,7 @@ class AreaPage extends Component {
                     height='300px'
                     zoom={15}
                 />
-     
                 <Footer />
-               // </Router>
             </div>
         )
     }
