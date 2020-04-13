b871e0924f30cf5052aecf238b0348730fd3f00d
tiffanyhsu03 <54927215+tiffanyhsu03@users.noreply.github.com>
2020-04-13 00:12:11 -0400

    added some login validation
    added login validation before viewing account page

--- midpoint-app/front-end/src/components/pages/LoginPage.js
+++ midpoint-app/front-end/src/components/pages/LoginPage.js
@@ -2,18 +2,22 @@ import React from "react";
 import LoginForm from '../forms/LoginForm'
 import Footer from '../template/Footer';
 import Header from '../template/Header';
+import {withRouter, BrowserRouter } from 'react-router-dom';
+
+
 class LoginPage extends React.Component {
 
     submit = (data) => {
        // just to make sure we actually get data 
         console.log(data);
+        this.props.history.push('/account');
     };
 
     render() {
         return (
             <div>
                 <Header />
-                <h1>Login Page</h1>
+                {/* <h1 style={{textAlign: "center"}}>Welcome to Midpoint!</h1> */}
                 <LoginForm submit={this.submit}/>
                 <Footer />
             </div>
