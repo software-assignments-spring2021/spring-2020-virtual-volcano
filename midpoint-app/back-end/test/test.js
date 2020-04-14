4c96dec1ca319834761b9c8a232e98bfb848e93e
Niki <54921767+literallyniki@users.noreply.github.com>
2020-04-13 21:48:48 -0400

    check if algo returns a float

--- midpoint-app/back-end/test/test.js
+++ midpoint-app/back-end/test/test.js
@@ -1,6 +1,14 @@
 const app = require("../app");
 const chai = require("chai");
 const chaiHttp = require("chai-http");
+const algorithm = require("../app").algorithm;
 
 const {expect} = chai;
 chai.use(chaiHttp);
+
+describe('Algorithm', function(){
+  it('algorithm should return midpoint that is a float', function(){
+    let result = algorithm();
+    assert.typeOf(result, 'float');
+  });
+});
