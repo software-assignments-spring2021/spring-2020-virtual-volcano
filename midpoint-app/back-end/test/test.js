68fe1307cd049eb228e48ccd47e25ea7825dc90c
Niki <54921767+literallyniki@users.noreply.github.com>
2020-04-13 22:04:00 -0400

    Update test.js

--- midpoint-app/back-end/test/test.js
+++ midpoint-app/back-end/test/test.js
@@ -1,14 +1,26 @@
 const app = require("../app");
 const chai = require("chai");
 const chaiHttp = require("chai-http");
-const algorithm = require("../app").algorithm;
+//const algorithm = require("../app").algorithm;
 
 const {expect} = chai;
 chai.use(chaiHttp);
 
-describe('Algorithm', function(){
+describe("Algorithm", function(){
   it('algorithm should return midpoint that is a float', function(){
+    chai
     let result = algorithm();
     assert.typeOf(result, 'float');
   });
+  
+  describe("Login", () => {
+  it("Sends a 200 code when user information is passed", done => {
+    chai
+      .request(app)
+      .post("/login")
+      .send({email: "testEmail", password: "testPass"})
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        done();
+      });
 });
