1f748dcd8f0569eef926202e8e9edb5970ceb3d5
Valentine Choi <jhc591@nyu.edu>
2020-04-14 12:00:09 -0400

    Minor spacing revision

--- midpoint-app/front-end/src/components/forms/ResultForm.js
+++ midpoint-app/front-end/src/components/forms/ResultForm.js
@@ -1,34 +1,34 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { Form, Button, ButtonGroup,Dropdown, Icon  } from 'semantic-ui-react';
+import { Form, Button, ButtonGroup, Dropdown, Icon } from 'semantic-ui-react';
 import { Link } from "react-router-dom";
 import MapContainer from './MapContainer';
 import axios from 'axios';
 
 function mapsSelector(lat, lng) {
     if /* if we're on iOS, open in Apple Maps */
-      ((navigator.platform.indexOf("iPhone") != -1) || 
-       (navigator.platform.indexOf("iPod") != -1) || 
-       (navigator.platform.indexOf("iPad") != -1))
-      window.open("maps://maps.google.com/maps");
-  
-    else /* else use Google */{
-    //for directions
-    //let url = "https://maps.google.com/maps?daddr=<"+lat+">,<"+lng+">&amp;ll=";
-    //add query id parameter to get the place name
-    let url = "https://www.google.com/maps/search/?api=1&query="+lat+","+lng;
-    window.open(url);
+        ((navigator.platform.indexOf("iPhone") != -1) ||
+        (navigator.platform.indexOf("iPod") != -1) ||
+        (navigator.platform.indexOf("iPad") != -1))
+        window.open("maps://maps.google.com/maps");
+
+    else /* else use Google */ {
+        //for directions
+        //let url = "https://maps.google.com/maps?daddr=<"+lat+">,<"+lng+">&amp;ll=";
+        //add query id parameter to get the place name
+        let url = "https://www.google.com/maps/search/?api=1&query=" + lat + "," + lng;
+        window.open(url);
     }
-  }
+}
 
 function ResultForm(props) {
     //get midpoint
     const [data, setData] = useState([]);
     useEffect(() => {
-      axios.get('http://localhost:3000/result').then(function(response) {
-              setData(response.data);
-              console.log(response.data)
-      });
+        axios.get('http://localhost:3000/result').then(function (response) {
+            setData(response.data);
+            console.log(response.data)
+        });
     }, []);
 
     const resultOptions = [
@@ -56,20 +56,20 @@ function ResultForm(props) {
 
             <h3>Get Directions:</h3>
             <Dropdown className='dropdown'
-            placeholder = "Select Option"
-            fluid
-            selection
-            options={resultOptions}
-            style={{ width: "25%",left: "48%", right: "auto", transform: "translate(-50%,0)", float: "left"}}
+                placeholder="Select Option"
+                fluid
+                selection
+                options={resultOptions}
+                style={{ width: "25%", left: "48%", right: "auto", transform: "translate(-50%,0)", float: "left" }}
             />
-            <Button 
-            animated style = {{float: "right", marginRight: "35%", position: "fixed"}} 
-            onClick={()=>{mapsSelector(data.lat, data.lng)}}
-//            onClick={()=>{mapsSelector()}}
+            <Button
+                animated style={{ float: "right", marginRight: "35%", position: "fixed" }}
+                onClick={() => { mapsSelector(data.lat, data.lng) }}
+            //            onClick={()=>{mapsSelector()}}
             >
                 <Button.Content visible>Go</Button.Content>
                 <Button.Content hidden>
-                    <Icon name='arrow right'/>
+                    <Icon name='arrow right' />
                 </Button.Content>
             </Button>
 
