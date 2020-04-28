27e0df0265851638b32f097a0a0cedc0604cd9ad
tiadizon <33070736+tiadizon@users.noreply.github.com>
2020-04-28 04:48:29 -0500

    adjust result page link to google maps

--- midpoint-app/front-end/src/components/forms/ResultForm.js
+++ midpoint-app/front-end/src/components/forms/ResultForm.js
@@ -26,11 +26,11 @@ function ResultForm(props) {
     const [data, setData] = useState([]);
     let midpoint_name = '';
     useEffect(() => {
-        axios.get('http://localhost:3000/name').then(function (response) {
+        axios.get('http://localhost:3000/result').then(function (response) {
             setData(response.data);
             console.log("this is name response data");
             console.log(response.data);
-            midpoint_name = response.data
+            // midpoint_name = response.data
 
         });
     }, []);
